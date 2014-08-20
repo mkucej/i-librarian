454fab15fd9b5a028030e332621c299f3ecb4e2d
Martin Kucej <i.librarian.software@gmail.com>
2014-08-19 23:57:43 -0500

    change menu

--- batchimport.php
+++ batchimport.php
@@ -71,26 +71,26 @@ if (!isset($_GET['commence'])) {
             </div>
             <div style="clear:both"></div>
             <div class="item-sticker ui-widget-content ui-corner-all" style="width:600px;margin:auto">
-                <div class="alternating_row items ui-corner-top">
-                    Import PDF files from directory:<br>
-                    <button class="open-dirs-button" title="Browse directories"><i class="fa fa-folder-open"></i></button>
-                    <form id="batchimportform" action="batchimport.php" method="GET">
+                <form id="batchimportform" action="batchimport.php" method="GET">
+                    <div class="alternating_row items ui-corner-top">
+                        Import PDF files from directory:<br>
+                        <button class="open-dirs-button" title="Browse directories"><i class="fa fa-folder-open"></i></button>
                         <input type="text" name="directory" value="<?php print $batchimport_dir; ?>" size="45" style="width:510px"><br>
                         <table cellspacing=0 style="margin: 2px 0px 2px 0px">
                             <tr>
                                 <td class="select_span">
                                     <input type="checkbox" name="recursive" value="1" style="display:none" <?php print empty($batchimport_recursive) ? '' : 'checked'  ?>>
-                                    <i class="fa fa-<?php print empty($batchimport_recursive) ? 'check-square' : 'square-o'  ?>"></i>
+                                    <i class="fa fa-<?php print empty($batchimport_recursive) ? 'square-o' : 'check-square'  ?>"></i>
                                     Include files in subdirectories.
                                 </td>
                             </tr>
                         </table>
-                </div>
-                <div class="separator" style="margin:0"></div>
-                <div class="alternating_row items ui-corner-bottom">
-                    <button id="batchimportbutton"><i class="fa fa-save"></i> Import</button>
-                    </form>
-                </div>
+                    </div>
+                    <div class="separator" style="margin:0"></div>
+                    <div class="alternating_row items ui-corner-bottom">
+                        <button id="batchimportbutton"><i class="fa fa-save"></i> Import</button>
+                    </div>
+                </form>
             </div>
             <?php
         } else {
@@ -114,14 +114,18 @@ if (!isset($_GET['commence'])) {
 
             $pdf_files = array();
 
-            $lit = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($_GET['directory']), RecursiveIteratorIterator::LEAVES_ONLY, RecursiveIteratorIterator::CATCH_GET_CHILD);
-            $lit->rewind();
-            while ($lit->valid()) {
-                $file = $lit->key();
-                if (is_readable($file) && pathinfo($file, PATHINFO_EXTENSION) == 'pdf') {
-                    $pdf_files[] = $file;
+            if (isset($_GET['recursive'])) {
+                $lit = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($_GET['directory']), RecursiveIteratorIterator::LEAVES_ONLY, RecursiveIteratorIterator::CATCH_GET_CHILD);
+                $lit->rewind();
+                while ($lit->valid()) {
+                    $file = $lit->key();
+                    if (is_readable($file) && pathinfo($file, PATHINFO_EXTENSION) == 'pdf') {
+                        $pdf_files[] = $file;
+                    }
+                    $lit->next();
                 }
-                $lit->next();
+            } else {
+                $pdf_files = glob($_GET['directory'] . DIRECTORY_SEPARATOR . '*.pdf', GLOB_NOSORT);
             }
 
             $pdf_count = count($pdf_files);
@@ -207,7 +211,8 @@ if (!isset($_GET['commence'])) {
                                             &nbsp;<i class="fa fa-<?php print (isset($batchimport_database_pubmed) && $batchimport_database_pubmed == '1') ? 'check-square' : 'square-o'  ?>"></i> PubMed (biomedicine)
                                         </td>
                                     </tr>
-                                <?php }
+                                    <?php
+                                }
                                 if (!isset($_SESSION['remove_nasaads'])) {
                                     ?>
                                     <tr>
@@ -215,7 +220,7 @@ if (!isset($_GET['commence'])) {
                                             &nbsp;<i class="fa fa-<?php print (isset($batchimport_database_nasaads) && $batchimport_database_nasaads == '1') ? 'check-square' : 'square-o'  ?>"></i> NASA ADS (physics, astronomy)
                                         </td>
                                     </tr>
-            <?php } ?>
+                                <?php } ?>
                                 <tr>
                                     <td class="select_span"><input type="checkbox" name="database_crossref" value="1" style="display:none" <?php print (isset($batchimport_database_crossref) && $batchimport_database_crossref == '1') ? 'checked' : ''  ?>>
                                         &nbsp;<i class="fa fa-<?php print (isset($batchimport_database_crossref) && $batchimport_database_crossref == '1') ? 'check-square' : 'square-o'  ?>"></i> CrossRef (other sciences)
@@ -360,14 +365,18 @@ if (!isset($_GET['commence'])) {
 
     $files = array();
 
-    $lit = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($_GET['directory']), RecursiveIteratorIterator::LEAVES_ONLY, RecursiveIteratorIterator::CATCH_GET_CHILD);
-    $lit->rewind();
-    while ($lit->valid()) {
-        $file = $lit->key();
-        if (is_readable($file) && pathinfo($file, PATHINFO_EXTENSION) == 'pdf') {
-            $files[] = $file;
+    if (!empty($_GET['recursive'])) {
+        $lit = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($_GET['directory']), RecursiveIteratorIterator::LEAVES_ONLY, RecursiveIteratorIterator::CATCH_GET_CHILD);
+        $lit->rewind();
+        while ($lit->valid()) {
+            $file = $lit->key();
+            if (is_readable($file) && pathinfo($file, PATHINFO_EXTENSION) == 'pdf') {
+                $files[] = $file;
+            }
+            $lit->next();
         }
-        $lit->next();
+    } else {
+        $files = glob($_GET['directory'] . DIRECTORY_SEPARATOR . '*.pdf', GLOB_NOSORT);
     }
 
     $stopwords = "a's, able, about, above, according, accordingly, across, actually, after, afterwards, again, against, ain't, all, allow, allows, almost, alone, along, already, also, although, always, am, among, amongst, an, and, another, any, anybody, anyhow, anyone, anything, anyway, anyways, anywhere, apart, appear, appreciate, appropriate, are, aren't, around, as, aside, ask, asking, associated, at, available, away, awfully, be, became, because, become, becomes, becoming, been, before, beforehand, behind, being, believe, below, beside, besides, best, better, between, beyond, both, brief, but, by, c'mon, c's, came, can, can't, cannot, cant, cause, causes, certain, certainly, changes, clearly, co, com, come, comes, concerning, consequently, consider, considering, contain, containing, contains, corresponding, could, couldn't, currently, definitely, described, despite, did, didn't, different, do, does, doesn't, doing, don't, done, down, during, each, edu, eg, either, else, elsewhere, enough, entirely, especially, et, etc, even, ever, every, everybody, everyone, everything, everywhere, ex, exactly, example, except, far, few, followed, following, follows, for, former, formerly, from, further, furthermore, get, gets, getting, given, gives, go, goes, going, gone, got, gotten, greetings, had, hadn't, happens, hardly, has, hasn't, have, haven't, having, he, he's, hello, help, hence, her, here, here's, hereafter, hereby, herein, hereupon, hers, herself, hi, him, himself, his, hither, hopefully, how, howbeit, however, i'd, i'll, i'm, i've, ie, if, in, inasmuch, inc, indeed, indicate, indicated, indicates, inner, insofar, instead, into, inward, is, isn't, it, it'd, it'll, it's, its, itself, just, keep, keeps, kept, know, knows, known, last, lately, later, latter, latterly, least, less, lest, let, let's, like, liked, likely, little, look, looking, looks, ltd, mainly, many, may, maybe, me, mean, meanwhile, merely, might, more, moreover, most, mostly, much, must, my, myself, name, namely, nd, near, nearly, necessary, need, needs, neither, never, nevertheless, new, next, no, nobody, non, none, noone, nor, normally, not, nothing, novel, now, nowhere, obviously, of, off, often, oh, ok, okay, old, on, once, ones, only, onto, or, other, others, otherwise, ought, our, ours, ourselves, out, outside, over, overall, own, particular, particularly, per, perhaps, placed, please, possible, presumably, probably, provides, que, quite, qv, rather, rd, re, really, reasonably, regarding, regardless, regards, relatively, respectively, right, said, same, saw, say, saying, says, secondly, see, seeing, seem, seemed, seeming, seems, seen, self, selves, sensible, sent, serious, seriously, several, shall, she, should, shouldn't, since, so, some, somebody, somehow, someone, something, sometime, sometimes, somewhat, somewhere, soon, sorry, specified, specify, specifying, still, sub, such, sup, sure, t's, take, taken, tell, tends, th, than, thank, thanks, thanx, that, that's, thats, the, their, theirs, them, themselves, then, thence, there, there's, thereafter, thereby, therefore, therein, theres, thereupon, these, they, they'd, they'll, they're, they've, think, this, thorough, thoroughly, those, though, through, throughout, thru, thus, to, together, too, took, toward, towards, tried, tries, truly, try, trying, twice, un, under, unfortunately, unless, unlikely, until, unto, up, upon, us, use, used, useful, uses, using, usually, value, various, very, via, viz, vs, want, wants, was, wasn't, way, we, we'd, we'll, we're, we've, welcome, well, went, were, weren't, what, what's, whatever, when, whence, whenever, where, where's, whereafter, whereas, whereby, wherein, whereupon, wherever, whether, which, while, whither, who, who's, whoever, whole, whom, whose, why, will, willing, wish, with, within, without, won't, wonder, would, would, wouldn't, yes, yet, you, you'd, you'll, you're, you've, your, yours, yourself, yourselves";
@@ -610,7 +619,7 @@ if (!isset($_GET['commence'])) {
                     }
 
                     if (!empty($title)) {
-                        
+
                         database_connect($database_path, 'library');
 
                         if (!empty($authors))
@@ -668,7 +677,7 @@ if (!isset($_GET['commence'])) {
                         $last_insert = null;
                         $id = $last_row['last_insert_rowid()'];
                         $new_file = $last_row['max(file)'];
-                        
+
                         if (isset($_GET['shelf']) && !empty($userID)) {
                             $user_query = $dbHandle->quote($userID);
                             $file_query = $dbHandle->quote($id);
